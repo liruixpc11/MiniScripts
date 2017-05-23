dba4cd07fbacd305d4ab50f7425b91833e57d7c6
Li Rui <lirui3252007012@gmail.com>
2017-05-23 15:53:07 +0800

    add zsh.

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -4,6 +4,15 @@
 apt update && apt upgrade
 apt install -y build-essential git curl wget
 
+# install oh-my-zsh
+apt install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+export ZSH_CUSTOM=~/.oh-my-zsh/custom
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sed -i 's/^ZSH_THEME=.*/ZSH_THEME="ys"/g' ~/.zshrc
+sed -i 's/^plugins=.*/plugins=\(git zsh-autosuggestions zsh-syntax-highlighting\)/g' ~/.zshrc
+
 # install Java and Maven
 apt install -y openjdk-8-jdk maven
 mkdir ~/.m2
@@ -20,6 +29,7 @@ cat >~/.m2/settings.xml <<EOF
     </mirrors>
 </settings>
 EOF
+echo >>~/.zshrc "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64"
 
 # install Python
 apt install -y python python-pip python-dev
@@ -34,11 +44,21 @@ EOF
 pip install -U pip setuptools
 pip install -U "ipython<6" numpy scipy pandas matplotlib jupyter
 
-# install oh-my-zsh
-apt install -y zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-export ZSH_CUSTOM=~/.oh-my-zsh/custom/plugins
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sed -i 's/^ZSH_THEME=.*/ZSH_THEME="ys"/g' ~/.zshrc
-sed -i 's/^plugins=.*/plugins=\(git zsh-autosuggestions zsh-syntax-highlighting\)/g' ~/.zshrc
+# install vim
+apt install -y vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cat >~/.vimrc <<EOF
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+set autoindent
+set smartindent
+
+set fileencodings=utf-8,gbk
+
+autocmd FileType python set expandtab
+
+execute pathogen#infect()
+syntax on
+filetype plugin indent on
+EOF
